5e6811d5ff47a0ee85534d226742480aa6a92c21
Alexey Utkin <alexey.utkin@gmail.com>
2022-07-28 18:53:50 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include <stdlib.h>
 #include "../lib/libfunc.h"
 
-int foo(int x) {
+
+
+int foo12(int x) {
     return 1/x;
 }
 
+
+int opp(int x) {
+    return foo12(x);
+}
+
+
 int main(int argc, char* argv[]) {
-    foo(0);
+    foo12(0);
     if (argc == 2) {
         int k = atoi(argv[1]);
         if (k == 1) {
